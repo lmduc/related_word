1a56107ef7629558cfa48690b16b7bab373fb9ba
MatBi <leminhducktvn@gmail.com>
2016-04-06 01:13:27 +0800

    Complete gem

--- lib/related_word.rb
+++ lib/related_word.rb
@@ -1,12 +1,13 @@
 require "related_word/version"
-require "nokogiri"
+require "json"
 require "net/http"
 require "uri"
 
 module RelatedWord
   def self.find(word)
-    link = "http://semantic-link.com/#/#{word}"
+    link = "http://semantic-link.com/related.php?word=#{word}"
     uri = URI.parse(link)
     response = Net::HTTP.get_response(uri)
+    JSON.parse(response)
   end
 end
--- related_word.gemspec
+++ related_word.gemspec
@@ -22,6 +22,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
-  spec.add_development_dependency "nokogiri", "~> 1.6"
-  spec.add_development_dependency "byebug", "~> 8.2"
+  spec.add_development_dependency "json", "~> 1.6"
 end
